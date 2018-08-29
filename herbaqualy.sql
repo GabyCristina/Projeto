e51015d104f576ed6dd18c20a201d275bf86aec8
GabyCristina <gabrilacidral@gmail.com>
2018-08-29 13:24:14 -0300

    Add files via upload

---
+++ herbaqualy.sql
@@ -0,0 +1,291 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 16-Ago-2018 às 02:08
+-- Versão do servidor: 10.1.34-MariaDB
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `herbaqualy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `descricao_cate` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `categoria`
+--
+
+INSERT INTO `categoria` (`id_categoria`, `descricao_cate`) VALUES
+(1, 'cha'),
+(2, 'fruta\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `id_comentario` int(11) NOT NULL,
+  `data_hota` datetime DEFAULT NULL,
+  `texto_comen` varchar(7000) DEFAULT NULL,
+  `id_pergunta` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pergunta`
+--
+
+CREATE TABLE `pergunta` (
+  `detalhamento_per` varchar(7000) DEFAULT NULL,
+  `pergunta` varchar(100) DEFAULT NULL,
+  `id_pergunta` int(11) NOT NULL,
+  `data_hora` datetime DEFAULT CURRENT_TIMESTAMP,
+  `id_user` int(11) DEFAULT NULL,
+  `id_categoria` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `pergunta`
+--
+
+INSERT INTO `pergunta` (`detalhamento_per`, `pergunta`, `id_pergunta`, `data_hora`, `id_user`, `id_categoria`) VALUES
+('1', '1', 1, '2018-08-15 00:00:00', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `planta`
+--
+
+CREATE TABLE `planta` (
+  `id_planta` int(11) NOT NULL,
+  `titulo` varchar(100) DEFAULT NULL,
+  `descricao_plan` varchar(7000) DEFAULT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `resposta`
+--
+
+CREATE TABLE `resposta` (
+  `id_resposta` int(11) NOT NULL,
+  `data_hora` datetime DEFAULT NULL,
+  `texto_res` varchar(7000) DEFAULT NULL,
+  `id_pergunta` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tip_user`
+--
+
+CREATE TABLE `tip_user` (
+  `id_tip_user` int(11) NOT NULL,
+  `descricao_tip` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tip_user`
+--
+
+INSERT INTO `tip_user` (`id_tip_user`, `descricao_tip`) VALUES
+(1, 'admin'),
+(2, 'comum\r\n');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_user` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `login` varchar(50) DEFAULT NULL,
+  `senha` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `id_tip_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_user`, `nome`, `login`, `senha`, `email`, `id_tip_user`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin@admin', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indexes for table `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `id_pergunta` (`id_pergunta`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `pergunta`
+--
+ALTER TABLE `pergunta`
+  ADD PRIMARY KEY (`id_pergunta`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
+-- Indexes for table `planta`
+--
+ALTER TABLE `planta`
+  ADD PRIMARY KEY (`id_planta`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
+-- Indexes for table `resposta`
+--
+ALTER TABLE `resposta`
+  ADD PRIMARY KEY (`id_resposta`),
+  ADD KEY `id_pergunta` (`id_pergunta`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `tip_user`
+--
+ALTER TABLE `tip_user`
+  ADD PRIMARY KEY (`id_tip_user`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_tip_user` (`id_tip_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pergunta`
+--
+ALTER TABLE `pergunta`
+  MODIFY `id_pergunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `planta`
+--
+ALTER TABLE `planta`
+  MODIFY `id_planta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `resposta`
+--
+ALTER TABLE `resposta`
+  MODIFY `id_resposta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tip_user`
+--
+ALTER TABLE `tip_user`
+  MODIFY `id_tip_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`id_pergunta`) REFERENCES `pergunta` (`id_pergunta`),
+  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`);
+
+--
+-- Limitadores para a tabela `pergunta`
+--
+ALTER TABLE `pergunta`
+  ADD CONSTRAINT `pergunta_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`),
+  ADD CONSTRAINT `pergunta_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`);
+
+--
+-- Limitadores para a tabela `planta`
+--
+ALTER TABLE `planta`
+  ADD CONSTRAINT `planta_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`),
+  ADD CONSTRAINT `planta_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`);
+
+--
+-- Limitadores para a tabela `resposta`
+--
+ALTER TABLE `resposta`
+  ADD CONSTRAINT `resposta_ibfk_1` FOREIGN KEY (`id_pergunta`) REFERENCES `pergunta` (`id_pergunta`),
+  ADD CONSTRAINT `resposta_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`);
+
+--
+-- Limitadores para a tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_tip_user`) REFERENCES `tip_user` (`id_tip_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
